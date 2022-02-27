04862034b023f7489b56503e36867291236dc9e7
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-02-27 14:33:13 +0530

    added table to home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,62 +1,39 @@
 import { NavBar } from "../NavBar/NavBar";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
 import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
+import { DataGrid } from "@mui/x-data-grid";
 import * as api from "../../service/api";
 export function Home() {
 	let [trees, setTrees] = useState([]);
 	let [isLoading, setIsLoading] = useState(true);
-	const Item = styled(Paper)(({ theme }) => ({
-		backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-		...theme.typography.body2,
-		padding: theme.spacing(2),
-		textAlign: "center",
-		borderColor: "black",
-		color: theme.palette.text.secondary,
-	}));
+	let columns = [
+		{ field: "id", headerName: "ID", width: 10 },
+		{ field: "tree_name", headerName: "Tree Name", width: 200 },
+		{ field: "created_at", headerName: "Created At", width: 200 },
+		{ field: "deleted_at", headerName: "Deleted At", width: 200 },
+		{ field: "edit", headerName: "Edit", width: 200 },
+	];
 
 	useEffect(() => {
 		const fetchData = async () => {
-			// api.getAllTrees(1).then((data) => {
-			// 	console.log(data);
-			// 	setTrees(data.rows);
-			// 	setIsLoading(false);
-			// });
-			let data =await api.getAllTrees(1);
-			console.log(data);
+			let data = await api.getAllTrees(1);
 			setTrees(data.rows);
-			const trees_count = data.count;
 			setIsLoading(false);
 		};
 		fetchData();
 	}, []);
-	useEffect(() => {
-		console.log(trees);
-	}, [trees]);
+	trees?.forEach((tree) => {});
 	return (
 		<div className="">
 			<NavBar isLoading={isLoading}></NavBar>
-			<Box sx={{ flexGrow: 1 }}>
-				<Card spacing={{ xs: 2, md: 3 }} columns={{ xs: 6, sm: 8, md: 12 }}>
-					{trees?.map((tree) => {
-						return (
-							<Grid
-								item
-								xs={12}
-								sm={4}
-								md={4}
-								key={tree.id}
-								style={{ border: "2px" }}
-							>
-								<Item>{tree.tree_name}</Item>
-							</Grid>
-						);
-					})}
-				</Card>
+			<Box style={{}}>
+				<DataGrid
+					rows={trees}
+					columns={columns}
+					columnBufer={2}
+					autoHeight
+					fcolumnThreshold={2}
+				/>
 			</Box>
 		</div>
 	);
