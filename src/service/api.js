5b4c5b8b786638f3f4fd0abf322079e951235884
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-02-28 12:11:26 +0530

    added api logger

--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,17 @@
 const axios = require("axios");
+axios.interceptors.request.use((request) => {
+	console.info("Request:", JSON.stringify(request, null, 2));
+	return request;
+});
+
+axios.interceptors.response.use((response) => {
+	console.info("Response:", JSON.stringify(response, null, 2));
+	return response;
+});
 const url = process.env.REACT_APP_API_BASE_URL;
 export const createUser = async (user) => {
-	let result = await axios.post(url + "/users", {
-		user,
-	});
+	console.log(user);
+	let result = await axios.post(url + "/users",user);
 	return result;
 };
 
@@ -11,3 +19,5 @@ export const getAllTrees = async (user_id) => {
 	let result = await axios.get(url + "/tree/all/" + user_id);
 	return result.data;
 };
+
+
