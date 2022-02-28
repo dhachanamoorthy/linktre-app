b4bcef7dfe2fe664c62f5c3cd5d1f588aa8ffae9
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-02-28 12:16:03 +0530

    fixed create user api call issue

--- src/service/SessionManager.js
+++ src/service/SessionManager.js
@@ -1,27 +1,30 @@
 import { auth } from "./firebase-config";
-import {createUser} from './api.js'
-export const login = (user,token) => {
-    localStorage.setItem('user',user)
-    localStorage.setItem('auth_token',token);
-    console.log(user);
-    let payload = {
-      username:user?.displayName,
-      email:user?.email,
-      password:null
-    }
-    createUser(payload);
-}
+import { createUser } from "./api.js";
+export const login = async (user, token) => {
+	localStorage.setItem("user", user);
+	localStorage.setItem("auth_token", token);
+	console.log(user);
+	let payload = {
+		uuid: user?.uid,
+		username: user?.displayName,
+		email: user?.email,
+		mobile:user?.phoneNumber,
+		image_url: user?.photoURL,
+	};
+	console.log(payload);
+	let result = await createUser(payload);
+	console.log(result);
+};
 export const isLogin = () => {
-    if (localStorage.getItem('auth_token')) {
-        return true;
-    }
+	if (localStorage.getItem("auth_token")) {
+		return true;
+	}
 
-    return false;
-}
+	return false;
+};
 
-export const logout= () =>{
-    console.log('logged out');
-    localStorage.removeItem('user');
-    localStorage.removeItem('auth_token');
-    auth.signOut();
-}
+export const logout = () => {
+	localStorage.removeItem("user");
+	localStorage.removeItem("auth_token");
+	auth.signOut();
+};
