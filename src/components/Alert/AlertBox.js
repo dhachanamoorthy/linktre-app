bd6d9277b74b02bfe56dd13f353a1408e95df5ca
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-03-01 21:53:03 +0530

    added alert box animation

--- src/components/Alert/AlertBox.js
+++ src/components/Alert/AlertBox.js
@@ -1,9 +1,14 @@
 import { useState, forwardRef } from "react";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
+import Slide from '@mui/material/Slide';
 
 const Alert = forwardRef(function Alert(props, ref) {
 	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export function AlertBox(props) {
@@ -20,7 +25,7 @@ export function AlertBox(props) {
 
 	return (
 		<div>
-			<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+			<Snackbar open={open} TransitionComponent={Transition} autoHideDuration={6000} onClose={handleClose}>
 				<Alert
 					onClose={handleClose}
 					severity={data.severnity}
