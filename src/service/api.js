8b7b75870b32f5bd678b25a74a1a2a48c21383df
dhachanamoothy <dhachanamoothy3@gmail.com>
2022-03-14 23:16:51 +0530

    removed console logs

--- src/service/api.js
+++ src/service/api.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 axios.interceptors.request.use((request) => {
-  console.info("Request:", JSON.stringify(request, null, 2));
+  // console.info("Request:", JSON.stringify(request, null, 2));
   return request;
 });
 
 axios.interceptors.response.use((response) => {
-  console.info("Response:", JSON.stringify(response, null, 2));
+  // console.info("Response:", JSON.stringify(response, null, 2));
   return response;
 });
 const url = process.env.REACT_APP_API_BASE_URL;
