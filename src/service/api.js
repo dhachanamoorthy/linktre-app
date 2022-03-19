2e255531f52aa49eb4bf7e9084df04546d796efc
dhachanamoothy <dhachanamoothy3@gmail.com>
2022-03-19 17:39:11 +0530

    added auth token in request header

--- src/service/api.js
+++ src/service/api.js
@@ -1,14 +1,32 @@
+import { getStorage } from "./storage";
+import { STORAGE } from "../constants/storage.constants";
 const axios = require("axios");
+const url = process.env.REACT_APP_API_BASE_URL;
 axios.interceptors.request.use((request) => {
   // console.info("Request:", JSON.stringify(request, null, 2));
+  const accessToken = getStorage(STORAGE.AUTH);
+  request.headers = {
+    Authorization: `Bearer ${accessToken}`,
+    Accept: "application/json",
+  };
   return request;
 });
 
-axios.interceptors.response.use((response) => {
-  // console.info("Response:", JSON.stringify(response, null, 2));
-  return response;
-});
-const url = process.env.REACT_APP_API_BASE_URL;
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async function (error) {
+    const originalRequest = error.config;
+    const accessToken = getStorage(STORAGE.AUTH);
+    if (error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      // const access_token = await refreshAccessToken();
+      axios.defaults.headers.common["Authorization"] = "Bearer " + accessToken;
+      return axios(originalRequest);
+    }
+  }
+);
 
 export const createUser = async (user) => {
   console.log(user);
@@ -60,14 +78,14 @@ export const updateLink = async (id, payload) => {
   return result.data;
 };
 
-export const getUser = async (id)=>{
-  let result = await axios.get(url+"/users/"+id);
+export const getUser = async (id) => {
+  let result = await axios.get(url + "/users/" + id);
   return result;
-}
+};
 
-export const updateUser = async (payload)=>{
+export const updateUser = async (payload) => {
   const id = payload.id;
   console.log(payload);
-  let result = await axios.patch(url+"/users/"+id,payload);
+  let result = await axios.patch(url + "/users/" + id, payload);
   return result;
-}
+};
